d84001d7dce1cf5705db19f793df57b194267705
jamesrooney-keele <60387443+jamesrooney-keele@users.noreply.github.com>
2022-06-08 17:01:42 +0100

    Final test added for validation rules for sb-10503

--- libSBOLj3/src/test/java/org/sbolstandard/core3/entity/test/ComponentTest.java
+++ libSBOLj3/src/test/java/org/sbolstandard/core3/entity/test/ComponentTest.java
@@ -187,6 +187,7 @@ public class ComponentTest extends TestCase {
         seqINCHI.setElements("InChI=1S/C2H6O/c1-2-3/h3H,2H2,1H3"); //ethanol
         TestUtil.validateIdentified(seqINCHI, 0);
         seqINCHI.setElements("InChI=1S/C6H8O6/c7-1-2(8)5-3(9)4(10)6(11)12-5/h2,5,7-10H,1H2/t2-,5+/m0/s1"); //L-ascorbic acid with InChI
+        TestUtil.validateIdentified(seqINCHI, 0);
     }
 
 }
